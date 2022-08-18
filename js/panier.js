7ca00f980f0e861f2958abf286d88b803dc8c20d
kevin-fl <kevinflamang@gmail.com>
2022-08-19 01:37:08 +0200

    update

---
+++ js/panier.js
@@ -0,0 +1,4 @@
+let produitEnregistrerDansLocalStorage = JSON.parse(localStorage.getItem("produit"));
+console.log(produitEnregistrerDansLocalStorage);
+
+//ln recup ce qui a dans le local storage et je peux le voir dans la console
