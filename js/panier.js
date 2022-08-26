b1fb8ddb7dff57713010e86bc277072c76612e01
kevin-fl <kevinflamang@gmail.com>
2022-08-26 02:22:29 +0200

    update cp still in progress

--- js/panier.js
+++ js/panier.js
@@ -30,7 +30,7 @@ if (produitEnregistrerDansLocalStorage === null || produitEnregistrerDansLocalSt
 
     let structureProduitPanier = [];
 
-    for ( k = 0; k < produitEnregistrerDansLocalStorage.length; k++) {
+    for (k = 0; k < produitEnregistrerDansLocalStorage.length; k++) {
         console.log(produitEnregistrerDansLocalStorage.length);
 
         structureProduitPanier = structureProduitPanier + `
@@ -65,34 +65,34 @@ let btn_supprimer = Array.from(document.querySelectorAll(".btn-supprimer"));
 console.log(btn_supprimer);
 
 //boucle for pr choper chaque produit present dans le panier 
-for ( let l = 0 ; l < btn_supprimer.length; l++) {
-    
+for (let l = 0; l < btn_supprimer.length; l++) {
+
     //instructions : btn_supprimer doit etre ecouter , methode addeventlistener .
     // fonction fleché qui donne les instructions  
-    
+
     btn_supprimer[l].addEventListener("click", (event) => {
-       event.preventDefault();
+        event.preventDefault();
 
-      let id_selectionner_delete = produitEnregistrerDansLocalStorage[l].id_produitSelectionner;
-      console.log(id_selectionner_delete);
-    
+        let id_selectionner_delete = produitEnregistrerDansLocalStorage[l].id_produitSelectionner;
+        console.log(id_selectionner_delete);
 
-      // avec la methode filter 
-      produitEnregistrerDansLocalStorage = produitEnregistrerDansLocalStorage.filter(el => el.id_produitSelectionner !== id_selectionner_delete); 
-console.log(produitEnregistrerDansLocalStorage);
-      
 
-// une fois que c est fait , on envoi la variable tranformer en format JSON dans le local storage avc methode setItem 
-localStorage.setItem("produit", JSON.stringify(produitEnregistrerDansLocalStorage));
+        // avec la methode filter 
+        produitEnregistrerDansLocalStorage = produitEnregistrerDansLocalStorage.filter(el => el.id_produitSelectionner !== id_selectionner_delete);
+        console.log(produitEnregistrerDansLocalStorage);
 
-//alert pr avettir produit supprimer du panier 
-alert("ce produit a ete supprimé de votre panier");
-window.location.href = "panier.html";
+
+        // une fois que c est fait , on envoi la variable tranformer en format JSON dans le local storage avc methode setItem 
+        localStorage.setItem("produit", JSON.stringify(produitEnregistrerDansLocalStorage));
+
+        //alert pr avettir produit supprimer du panier 
+        alert("ce produit a ete supprimé de votre panier");
+        window.location.href = "panier.html";
 
     })
 }
 
-    
+
 // tjrs creer le code html du button avant la methode insertAdjacentHTML sinon ca ne fonctionne pas !
 
 const btn_supprime_tout_html = `
@@ -101,7 +101,7 @@ const btn_supprime_tout_html = `
 //insertion du button ds le html du panier , car pr que le queryselector existe , faut que le button existe !
 //aller recup la class container-panier-vide dans le panier.js
 
-positionElement4.insertAdjacentHTML("beforeend",btn_supprime_tout_html);
+positionElement4.insertAdjacentHTML("beforeend", btn_supprime_tout_html);
 
 
 //↑cette methode prend en parametre beforebegin ou afterbengin opu beforened etc ... mdn :)
@@ -109,19 +109,19 @@ positionElement4.insertAdjacentHTML("beforeend",btn_supprime_tout_html);
 //la selection de la reference du bouton "btn-tous-supprimer-panier"
 
 const btn_supprime_tout_js = document.querySelector(".btn-supprime-tout");
-console.log(btn_supprime_tout_js);
+//console.log(btn_supprime_tout_js);
 
 //suppression de la key "produit" du localstorage pr vider entierement le panier 
 
-btn_supprime_tout_js.addEventListener('click' , (e)=> {
+btn_supprime_tout_js.addEventListener('click', (e) => {
     e.preventDefault();
     //.removeItem pr vider le local storage 
     localStorage.removeItem("produit");
 
     //alert panier a ete vidé 
-    
+
     alert("le panier a été vidé");
-    
+
     //rechargement de la page panier 
     window.location.href = "panier.html";
 });
@@ -137,34 +137,34 @@ let priceTotalCalculated = [];
 
 // aller chercher les prix ds le panier sous fiorme de array 
 
-for ( let p = 0 ; p <  produitEnregistrerDansLocalStorage.length ; p++){
-    let priceProduitPanier =  produitEnregistrerDansLocalStorage[p].price;
-   // console.log(priceProduitPanier); 
-   
-   //mettre les price du panier dans la var priceTotalCalculated
-   priceTotalCalculated.push(priceProduitPanier);
-   console.log(priceTotalCalculated);
-   
+for (let p = 0; p < produitEnregistrerDansLocalStorage.length; p++) {
+    let priceProduitPanier = produitEnregistrerDansLocalStorage[p].price;
+    // console.log(priceProduitPanier); 
+
+    //mettre les price du panier dans la var priceTotalCalculated
+    priceTotalCalculated.push(priceProduitPanier);
+    // console.log(priceTotalCalculated);
+
 }
 
 
 //additionner les prix dans le tableau priceTotalCalculated avec methode .reduce --> accumule les valeurs pour les reduire a une seule valeur 
 
 
-const reducer = (accumulator , currentValue) => accumulator + currentValue;
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
 //console.log(priceTotalCalculated.reduce(reducer));
 
-const priceTotal = priceTotalCalculated.reduce(reducer,0); // rajoute le 0 car si panier vide ce la va créer une erreur ;) 
+const priceTotal = priceTotalCalculated.reduce(reducer, 0); // rajoute le 0 car si panier vide ce la va créer une erreur ;) 
 //console.log(priceTotal);
 
 
- // &fficher le total dans le html 
+// &fficher le total dans le html 
 
- const affichagePriceHtml = `<div class="affichage-prix-html">le prix est de : ${priceTotal} $ </div>`
+const affichagePriceHtml = `<div class="affichage-prix-html">le prix est de : ${priceTotal} $ </div>`
 
- //injection Html apres le dernier enfant (a la suite de tout ceci )
- 
- positionElement4.insertAdjacentHTML("beforeend", affichagePriceHtml) ;
+//injection Html apres le dernier enfant (a la suite de tout ceci )
+
+positionElement4.insertAdjacentHTML("beforeend", affichagePriceHtml);
 
 
 
@@ -173,15 +173,15 @@ const priceTotal = priceTotalCalculated.reduce(reducer,0); // rajoute le 0 car s
 
 //-------------------------------------******************************FORMULAIRE ***********************************-------------------------------------------------------------------
 
- 
- //Le formulaire dans le html mais dans le js pour pouvoir interagir avec 
 
- const afficherFormulaireHtml = () => {
+//Le formulaire dans le html mais dans le js pour pouvoir interagir avec 
+
+const afficherFormulaireHtml = () => {
 
     //selection element du DOM pour le formulaire 
 
     const positionElement5 = document.querySelector("#container-produits-panier");
-   
+
     const structureFormulaire = `
     <!--Le formulaire dans le html -->
             
@@ -201,12 +201,15 @@ const priceTotal = priceTotalCalculated.reduce(reducer,0); // rajoute le 0 car s
         
         <label for="ville">Ville</label>
         <input type="text" id="ville" name="ville">
-        
+
+        <label for="codePostale">Code Postale</label>
+        <input type="number" id="codePostale" name="codePostale" placeholder ="number">
+
         <label for="telephone">telephone</label>
         <input type="text" id="telephone" name="telephone" placeholder="champ obligatoire">
         
         <label for="email">E-mail</label>
-        <input type="text" id="email" name="email" placeholder="champ obligatoire">
+        <input type="email" id="email" name="email" placeholder="champ obligatoire">
         <button id="envoyerform" type="submit" name="envoyerform">Valider la commande</button>
     </form>
     `;
@@ -215,80 +218,134 @@ const priceTotal = priceTotalCalculated.reduce(reducer,0); // rajoute le 0 car s
 
     positionElement5.insertAdjacentHTML("afterend", structureFormulaire);
 
- };
+};
 
- // mnt ca va affichage le formulaire 
+// mnt ca va affichage le formulaire 
 
- afficherFormulaireHtml();
+afficherFormulaireHtml();
 
 //selection du bouton pour envoyer le formulaire 
 const btnEnvoyerForm = document.querySelector("#envoyerform");
 //console.log(btnEnvoyerForm);
 
 
- //addeventlistener pr ecouter du button formulaire
+//addeventlistener pr ecouter du button formulaire
 
- btnEnvoyerForm.addEventListener("click" , (e)=>{
+btnEnvoyerForm.addEventListener("click", (e) => {
     e.preventDefault();
 
-//recuperation des valeurs du formulaire
-/*const formulaireValues = {
-    prenom : document.querySelector("#prenom").value,
-    nom :    document.querySelector("#nom").value,  
-    adresse :   document.querySelector("#adresse").value,
-    ville : document.querySelector("#ville").value,
-    telephone : document.querySelector("#telephone").value,
-    email : document.querySelector("#email").value,
-}
-*/
-//console.log(formulaireValues);
-
-// const ln 235 autre moyen en creant une classe a la place de la const formulaireValues
-
-class Formulaire {
-    constructor(input){    // on laisse proprietes du constructor vide car document.queryselector . / si input ici alors input dans la ln 259 
-        // dans l exemple avec cet input , montre qu ont peut decide de prendre seulement une proprietes de l objet , exemple prendre que prenom 
-        // mais pour ca faut tt mettre en greenscreen , a voir pq je n ai pas encore compris 
-        this.prenom = document.querySelector("#prenom").value;
-        this.nom = document.querySelector("#nom").value;
-        this.adresse = document.querySelector("#adresse").value;
-        this.ville = document.querySelector("#ville").value;
-        this.telephone = document.querySelector("#telephone").value;
-        this.email = document.querySelector("#email").value;
-        //truc sympa ↓ avec litteraux de gabarits
-       // this.input = document.querySelector(`#${input}`).value;
+    //recuperation des valeurs du formulaire
+    /*const formulaireValues = {
+        prenom : document.querySelector("#prenom").value,
+        nom :    document.querySelector("#nom").value,  
+        adresse :   document.querySelector("#adresse").value,
+        ville : document.querySelector("#ville").value,
+        telephone : document.querySelector("#telephone").value,
+        email : document.querySelector("#email").value,
     }
-}
+    */
+    //console.log(formulaireValues);
 
-//appel de l instance de la Class Formulaire pr créer l objet formulaireValues2
-const formulaireValues2 = new Formulaire("prenom");     //("prenom") par exemple pr juste recup le prenom 
+    // const ln 235 autre moyen en creant une classe a la place de la const formulaireValues
 
-//console.log("formulaireValues2");
-//console.log(formulaireValues2);
+    class Formulaire {
+        constructor(input) {    // on laisse proprietes du constructor vide car document.queryselector . / si input ici alors input dans la ln 259 
+            // dans l exemple avec cet input , montre qu ont peut decide de prendre seulement une proprietes de l objet , exemple prendre que prenom 
+            // mais pour ca faut tt mettre en greenscreen , a voir pq je n ai pas encore compris 
+            this.prenom = document.querySelector("#prenom").value;
+            this.nom = document.querySelector("#nom").value;
+            this.adresse = document.querySelector("#adresse").value;
+            this.ville = document.querySelector("#ville").value;
+            this.telephone = document.querySelector("#telephone").value;
+            this.email = document.querySelector("#email").value;
+            this.codePostale = document.querySelector("#codePostale").value;
+            //truc sympa ↓ avec litteraux de gabarits
+            // this.input = document.querySelector(`#${input}`).value;
+        }
+    }
 
-// mettre l objet formulaireValues dans le local storage 
-//recuperation des valeurs du formulaire  pour les mettres ds le local storage
-// NB : je recup les valeurs en storage car ce n est pas une vrai api , sinon j aurais utiliser fetch post ! ;)
+    //appel de l instance de la Class Formulaire pr créer l objet formulaireValues2
+    const formulaireValues2 = new Formulaire("prenom");     //("prenom") par exemple pr juste recup le prenom 
+
+    console.log("formulaireValues2");
+    console.log(formulaireValues2);
+
+    // mettre l objet formulaireValues dans le local storage 
+    //recuperation des valeurs du formulaire  pour les mettres ds le local storage
+    // NB : je recup les valeurs en storage car ce n est pas une vrai api , sinon j aurais utiliser fetch post ! ;)
 
 
-//-----------------****************************************GESTION VALIDATION DU FORMULAIRE********************************------------------------------------------------
-// ---------utilisation des regex--------------------- NB y a des createur de regex sur le net , plus simple
+    //-----------------****************************************GESTION VALIDATION DU FORMULAIRE********************************------------------------------------------------
+    // ---------utilisation des regex--------------------- NB y a des createur de regex sur le net , plus simple
 
-function prenomControle(){
+    const textAlert = (value) => {
+        return `${value} "chiffre et symbole pas autorisé ne pas depasser 20 charact merci"`;
 
-    // controle la validite du prenom 
-    const lePrenom = formulaireValues2.prenom;
-    if(/^[A-Za-z]{2,20}$/.test(lePrenom)){    // {2,20} entre min 2 et max 20 lettres , on appel ca les quantificateurs
-        console.log("ok");
-        return true; 
-    } else {
-        console.log("ko");
-        alert('chiffre et symbole ne sont pas autorisé ne pas depasser 20 charactere merci')
-        return false;
+    }
+
+    const textAlertCp =  (value) => {
+        return `${value}" seul les chiffres sont acceptés merci"`;
+    }
+
+    const regExCodePostale = (value) => {
+        return /^[0-9]{3,7}$/.test(value);
+    }
+    //↓ ceci s appelle une expression de fonction , on declare une const puis fonction fleché
+    const regExPrenomNomVille = (value) => {
+        return /^[A-Za-z]{2,20}$/.test(value);
+    }
+
+    function prenomControle() {
+        // controle la validite du prenom 
+        const lePrenom = formulaireValues2.prenom;
+        if (regExPrenomNomVille(lePrenom)) {    // {2,20} entre min 2 et max 20 lettres , on appel ca les quantificateurs
+            //console.log("ok");
+            return true;
+        } else {
+            //console.log("ko");
+            alert(textAlert("Prenom :"));
+            return false;
+        }
     };
-    console.log("lePrenom");
-    console.log(lePrenom);
-};
+
+    //console.log("lePrenom");
+    //console.log(lePrenom);
+
+    function nomControle() {
+        //controle validite du nom
+        const leNom = formulaireValues2.nom;
+        if (regExPrenomNomVille(leNom)) {
+            console.log("true nomcontrole");
+            return true;
+        } else {
+            alert(textAlert("Nom :"));
+            return false;
+        }
+    }
+
+    function codePostaleControle() {
+        //controle de la validite du cp
+        const leCodePostale = formulaireValues2.codePostale;
+        if (regExCodePostale(leCodePostale)){
+            console.log("code postale");
+        return true;
+        } else {
+            alert(textAlertCp("code Postale"));
+            return false;
+
+        }
+    }
+
+    // controle validité du formulaire avant envoi et surtt stockage dans le localstorage
+    if (prenomControle() && nomControle() && codePostaleControle) {
+        //mettre l objet formulaireValues2 ds le local storage
+        localStorage.setItem("formulaireValues2", JSON.stringify(formulaireValues2));  // va cree un objet formulaireValues mais pas ce qu on attend , car il a besoin d un string dans la methode . check l appli du localstorage ce sera ecrit object object .
+        //console.log('prenomControle');
+        //console.log(prenomControle());
+    } else {
+        // console.log(prenomControle());
+        alert("veuillez bien remplir le formulaire");
+    }
 
 
 
@@ -296,18 +353,17 @@ function prenomControle(){
 
 
 
-//↑---------------****************************FIN - GESTION VALIDATION DU FORMULAIRE**************************************-------------------------------------------------------------
 
 
 
 
 
-if(prenomControle()) {
+    //↑---------------****************************FIN - GESTION VALIDATION DU FORMULAIRE**************************************-------------------------------------------------------------
 
-localStorage.setItem("formulaireValues2", JSON.stringify(formulaireValues2));  // va cree un objet formulaireValues mais pas ce qu on attend , car il a besoin d un string dans la methode . check l appli du localstorage ce sera ecrit object object .
-} else{
-    alert("veuillez bien remplir le formulaire");
-}
+
+
+
+
 
 
 
@@ -317,17 +373,17 @@ localStorage.setItem("formulaireValues2", JSON.stringify(formulaireValues2));  /
     // mettre les valeurs du form et mettre les produits selectionnes dans l obet a envoyer vers le serveur 
     //2 objets que je met dans un objet 
 
-const aEnvoyer = {
-   produitEnregistrerDansLocalStorage,
-   //formulaireValues     // ln 235 soit cette methode soit avec class
-    //Formulaire
-}
-console.log("aEnvoyer");
-console.log(aEnvoyer);
+    const aEnvoyer = {
+        produitEnregistrerDansLocalStorage,
+        //formulaireValues     // ln 235 soit cette methode soit avec class
+        //Formulaire
+    }
+    console.log("aEnvoyer");
+    console.log(aEnvoyer);
 
-//envoie de l objet " aEnvoyer" vers le serveur 
+    //envoie de l objet " aEnvoyer" vers le serveur 
 
-});    
+});
 
 
 //----------------------**********************Mettre le contenu du local storage ds le champs du formulaire*****************-------------------- en gros que les données entrée ds le form reste la meme si je reactualise la page ou je vais chercher un autre produit 
@@ -355,7 +411,7 @@ console.log(dataLocalStorageObject);
 
 //3eme methode la meilleur et la + pro ! avec une fonction pr que le champ du formulaire soit rempli par les donnees du localstorage si elle existe
 
-function remplirInputDepuisLocalStorage(input){
+function remplirInputDepuisLocalStorage(input) {
     document.querySelector(`#${input}`).value = dataLocalStorageObject[input];
 };
 
@@ -370,4 +426,4 @@ remplirInputDepuisLocalStorage("ville");    // -> idem
 
 //--------------*******************Formulaire validation du champ avc conditions avt envoi ds local storage*********************---------------------------------
 
- 
+
